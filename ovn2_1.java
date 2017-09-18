bdf995e0bca02af25471d9ddee4105b1682e0aaf
David Andersson <anda16@nti.umea>
2017-09-18 10:24:27 +0200

    ovn2_1

---
+++ ovn2_1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class ovn2_1{
+	public static void main(String[] args){
+		String namn = "David Andersson";
+		String nummer = "\n070234234";
+		String adress = "\nDrottninggatan 1";
+		String info = namn + nummer + adress;
+		JOptionPane.showMessageDialog(null,info);
+	}
+}
