27ffb0aa7844588adf6c6f1943498ecfdef3b8bd
David Andersson <anda16@nti.umea>
2017-09-18 11:10:59 +0200

    ovn2_4

---
+++ ovn2_4.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+/* Uppgift 2.4
+David Andersson
+18/9 2017
+*/
+
+public class ovn2_4{
+	public static void main(String[] args){
+		
+		String namn = JOptionPane.showInputDialog("What is your first name?");
+		String namn2 = JOptionPane.showInputDialog("What is your last name?");
+		String halsning = "Hello " + namn + " " + namn2;
+		JOptionPane.showMessageDialog(null, halsning);
+		
+	}
+}
