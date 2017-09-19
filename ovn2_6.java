36d95ae075dc8f5ce90685e57749961069c981f5
David Andersson <anda16@nti.umea>
2017-09-19 12:24:21 +0200

    ovn2_6

---
+++ ovn2_6.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/* Uppgift 2.6
+David Andersson
+9/18 2017
+*/
+
+public class ovn2_6{
+	public static void main(String[] args){
+		char c1, c2;
+		c1 = '\u00D8';
+		c2 = '\u00F8';
+		JOptionPane.showMessageDialog(null,c1);
+		JOptionPane.showMessageDialog(null,c2);
+	}
+}
